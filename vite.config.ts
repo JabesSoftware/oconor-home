4e0bf7651abd664dd966e9ab74ac5c3486115789
G C <gabescallaghan@gmail.com>
2026-05-21 00:05:24 +1200

    updates to page redirects

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,22 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   root: '.',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        stories: 'pages/stories.html',
+        events: 'pages/events.html',
+        faq: 'pages/faq.html',
+        availability: 'pages/availability.html',
+        contact: 'pages/contact.html',
+        donate: 'pages/donate.html',
+        virtualTour: 'pages/virtual-tour.html',
+        projectOverview: 'pages/project-overview.html',
+        donors: 'pages/donors.html',
+        adminLogin: 'pages/admin-login.html',
+        adminEvents: 'pages/admin-events.html',
+      }
+    }
   }
 })
